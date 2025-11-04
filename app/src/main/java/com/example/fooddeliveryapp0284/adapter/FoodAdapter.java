ae71552ca69bc2173ff47c080c88d84a4058597b
AnuraagR9251 <anuraagr9251@gmail.com>
2025-11-04 20:20:33 +0530

    Menu is done

--- app/src/main/java/com/example/fooddeliveryapp0284/adapter/FoodAdapter.java
+++ app/src/main/java/com/example/fooddeliveryapp0284/adapter/FoodAdapter.java
@@ -5,22 +5,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.fooddeliveryapp0284.R;
 import com.example.fooddeliveryapp0284.model.FoodItem;
-
 import java.util.List;
 
 public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder> {
 
-    private List<FoodItem> foodItems;
+    private List<FoodItem> foodList;
 
-    public FoodAdapter(List<FoodItem> foodItems) {
-        this.foodItems = foodItems;
+    public FoodAdapter(List<FoodItem> foodList) {
+        this.foodList = foodList;
     }
 
     @NonNull
@@ -32,21 +28,15 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
-        FoodItem item = foodItems.get(position);
+        FoodItem item = foodList.get(position);
         holder.foodName.setText(item.getName());
-        holder.foodPrice.setText(String.format("$%.2f", item.getPrice()));
+        holder.foodPrice.setText((int) item.getPrice());
         holder.foodImage.setImageResource(item.getImageResource());
-
-        // Example: Handle click on the "Add" button
-        holder.itemView.findViewById(R.id.add_to_cart_button).setOnClickListener(v -> {
-            // Here you would add the item to the cart database
-            Toast.makeText(v.getContext(), item.getName() + " added to cart", Toast.LENGTH_SHORT).show();
-        });
     }
 
     @Override
     public int getItemCount() {
-        return foodItems.size();
+        return foodList.size();
     }
 
     public static class FoodViewHolder extends RecyclerView.ViewHolder {
@@ -59,6 +49,7 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder
             foodImage = itemView.findViewById(R.id.food_image);
             foodName = itemView.findViewById(R.id.food_name);
             foodPrice = itemView.findViewById(R.id.food_price);
+            // You can also find the "Add" button here and set a listener
         }
     }
 }
--- app/src/main/java/com/example/fooddeliveryapp0284/fragment/HomeFragment.java
+++ app/src/main/java/com/example/fooddeliveryapp0284/fragment/HomeFragment.java
@@ -1,9 +1,10 @@
-package com.example.fooddeliveryapp0284.fragments;
+package com.example.fooddeliveryapp0284.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,47 +12,46 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fooddeliveryapp0284.R;
-import com.example.fooddeliveryapp0284.adapter.FoodAdapter;
-import com.example.fooddeliveryapp0284.model.FoodItem;
+import com.example.fooddeliveryapp0284.adapter.HotelAdapter;
+import com.example.fooddeliveryapp0284.model.Hotel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    private RecyclerView recyclerView;
-    private FoodAdapter foodAdapter;
-    private List<FoodItem> foodItemList;
+    private RecyclerView hotelRecyclerView;
+    private HotelAdapter hotelAdapter;
+    private List<Hotel> hotelList;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // This inflates the fragment_home.xml layout you already have.
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // 1. Initialize RecyclerView
-        recyclerView = view.findViewById(R.id.food_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        hotelRecyclerView = view.findViewById(R.id.menu_recycler_view);
+        hotelRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        // 2. Prepare data with actual images
-        prepareFoodData();
+        // This method creates the specific list of 4 restaurants.
+        prepareHotelData();
 
-        // 3. Set up the adapter
-        foodAdapter = new FoodAdapter(foodItemList);
-        recyclerView.setAdapter(foodAdapter);
+        hotelAdapter = new HotelAdapter(hotelList);
+        hotelRecyclerView.setAdapter(hotelAdapter);
 
         return view;
     }
 
-    private void prepareFoodData() {
-        foodItemList = new ArrayList<>();
+    /**
+     * Prepares the list of 4 specific hotels as requested.
+     */
+    private void prepareHotelData() {
+        hotelList = new ArrayList<>();
 
-        // IMPORTANT: Replace these 'R.drawable' names with the actual names of your image files.
-        // For example, if you have 'burger.png', use 'R.drawable.burger'.
-        foodItemList.add(new FoodItem("Classic Burger", 9.99, R.drawable.burger)); // Example
-        foodItemList.add(new FoodItem("Margherita Pizza", 12.50, R.drawable.pizza)); // Example
-        foodItemList.add(new FoodItem("Caesar Salad", 7.25, R.drawable.salad));   // Example
-        foodItemList.add(new FoodItem("Spaghetti Bolognese", 11.00, R.drawable.pasta)); // Example
-        foodItemList.add(new FoodItem("Chocolate Brownie", 5.50, R.drawable.dessert)); // Example
-        foodItemList.add(new FoodItem("Vegan Wrap", 8.75, R.drawable.wrap));     // Example
+        // Make sure you have images named 'fast_food', 'south_indian', etc., in your drawable folder.
+        hotelList.add(new Hotel("Quick Bites", "Fast Food", R.drawable.fast_food));
+        hotelList.add(new Hotel("Dakshin Flavors", "South Indian", R.drawable.south_indian));
+        hotelList.add(new Hotel("The Salty Catch", "Sea Food", R.drawable.sea_food));
+        hotelList.add(new Hotel("Chaat Street", "Chats & Snacks", R.drawable.chats));
     }
 }
